3a8e14dd3920e4bf81aa2a61b499c1679b349940
Sonyaandr <andreevasd8@gmail.com>
2024-07-11 22:53:36 +0300

    Проверьте дз

---
+++ src/Book.java
@@ -0,0 +1,31 @@
+public class Book {
+    private String nameBook;
+    private String author;
+    private int yearPublic;
+
+    public Book(String nameBook, String author, int yearPublic) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.yearPublic = yearPublic;
+    }
+
+    public String getNameBook() {
+        return this.nameBook;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getYearPublic() {
+        return this.yearPublic;
+    }
+
+    public void setYearPublic(int yearPublic) {
+        this.yearPublic = yearPublic;
+    }
+@Override
+    public String toString() {
+        return "Название книги " + this.nameBook + ", автор " + this.author + ", Дата публикации " + this.yearPublic;
+    }
+}
